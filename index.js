745bfac8b5bbf253f85e61a4cbcd11eb2a0fbcc9
Joseph Abraham <ekurubal24@gmail.com>
2021-05-09 19:20:54 +0300

    Added null and number check

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ document.querySelector("#check_it").addEventListener("click", checkNumber);
 function checkNumber(){
     guess_count++;
     let userInputElement = document.querySelector("#nr_game");
+    let answerLabelElement = document.querySelector("#answer");
+
     document.querySelector("#count_it").innerText = `Guess count is: ${guess_count}`;
 
-    if(random_number == userInputElement.value){
-        document.querySelector("#answer").innerText = "You are the winner";
-    }
-    else{
-
-        if(random_number > userInputElement.value){
-            document.querySelector("#answer").innerText = "Try a higher number";
-    
-        } else if(random_number < userInputElement.value){
-            document.querySelector("#answer").innerText = "Try a smaller number";
-        } 
+    if (random_number == userInputElement.value){
+        answerLabelElement.innerText = "You are the winner";
+    } else if (userInputElement.value == ""){
+        answerLabelElement.innerText = "Enter a number";
+    } else if (random_number > userInputElement.value){
+        answerLabelElement.innerText = "Try a higher number";
+    } else if (random_number < userInputElement.value){
+        answerLabelElement.innerText = "Try a smaller number";
+    } else if (isNaN(userInputElement.value)){
+        answerLabelElement.innerText = "It's not a number";
     } 
-    //console.log(userInputElement.value);
+    console.log(userInputElement.value);
 }
 
-//console.log(random_number);
+console.log(random_number);
